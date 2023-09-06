3e6abce8fa479dad66ab3406476677af69ebf2dc
Jacob Li <ziyanli@amazon.com>
2023-09-06 13:36:42 -0700

    add extendedClient commit

---
+++ src/test/java/com/amazonaws/client/extendedClient/SqsExtendedClientExamplesTest.java
@@ -0,0 +1,11 @@
+package com.amazonaws.client.extendedClient;
+
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class SqsExtendedClientExamplesTest {
+
+}
